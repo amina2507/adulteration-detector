6ae6e6fd3d385098ee5b1c7c9d59676618012ce6
aminaashraf123 <95096566+aminaashraf123@users.noreply.github.com>
2025-05-06 21:54:56 +0530

    Updated ALLOWED_HOSTS for Render deployment

--- ripeness_detector/settings.py
+++ ripeness_detector/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4hhl-xw1du)2u$^++)yczgr+_)2_sepdy(^g4la^)bj(x!a=oq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['adulteration-detector.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 
