52d29862523355addb38505de28eb23acd3a10e6
aminaashraf123 <95096566+aminaashraf123@users.noreply.github.com>
2025-05-06 23:22:00 +0530

    Configure DATABASE_URL for PostgreSQL connection from Railway

--- ripeness_detector/settings.py
+++ ripeness_detector/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,16 +85,12 @@ WSGI_APPLICATION = 'ripeness_detector.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+load_dotenv()  # load .env file
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ripeness_detector',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    },
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 # Password validation
